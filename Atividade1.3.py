87fb6238d7baa9a0e3a9aee3d8239af97f7e2cb5
Johnie <Johnie.price@pm.me>
2024-09-26 11:37:23 -0300

    Atividade1.3 nome e media do aluno

--- Atividade1.3.py
+++ Atividade1.3.py
@@ -1,4 +1,6 @@
 # Desenvolver um fluxograma e um programa em Python que leia o nome de um
 # aluno e as notas das três provas que ele obteve no semestre. No final, informar o
 # nome do aluno e a sua média (aritmética).
-# #
+#
+
+'teste'
