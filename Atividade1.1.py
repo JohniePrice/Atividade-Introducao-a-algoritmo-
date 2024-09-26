4657a2e9a7458f14e17fac181b3a25149b634fce
Johnie <Johnie.price@pm.me>
2024-09-26 10:57:19 -0300

    Atividade1.1 Area do triangulo

---
+++ Atividade1.1.py
@@ -0,0 +1,9 @@
+#Desenvolver um fluxograma e um programa em Python que leia a base e a altura
+#de um triângulo e exiba sua área. AREA=BASE*ALTURA#
+
+base = float(input("Digite o valor para a Base do triangulo: "))
+altura = float(input("Digite o valor para a Base do triangulo: "))
+
+resultado = (base*altura)/2
+
+print("A Area do tringulo é: ",resultado)
